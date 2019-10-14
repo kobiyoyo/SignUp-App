87ba01253c96c0c0dd3448ec8b5a31f7d2ca6fdc
kobiyoyo <adamachubi@gmail.com>
2019-10-14 19:14:46 +0100

    added signup components

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
+  get 'users/new'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'pages#index'
   get '/help',    to: 'pages#help'
   get '/contact', to: 'pages#contact'
   get '/about',   to: 'pages#about'
-
+  get  '/signup',  to: 'users#new'
 end
