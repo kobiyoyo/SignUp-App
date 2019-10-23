af402d13340c35738b82ab23b69202f0687331c9
kobiyoyo <adamachubi@gmail.com>
2019-10-23 03:38:02 +0100

    added test files and vlidation

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -4,4 +4,29 @@ class UserTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
-end
+  def setup
+    @user = User.new(name: "Example User", email: "example@user.com")
+  end
+  test "this should be valid" do
+    assert @user.valid?
+  end
+
+  test "this should test for empty name" do
+    @user.name = " "
+    assert_not @user.valid?
+  end
+
+  test 'this should test for empty email' do
+    @user.email = " "
+    assert_not @user.valid?
+  end
+
+  test 'name should not be long' do
+    @user.name = "a" * 51
+    assert_not @user.valid?
+  end
+  test '' do
+    @user.email = "a" * 244 + "@gmail.com"
+  end
+end 
+
